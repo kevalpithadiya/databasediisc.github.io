5133d6a900e4d6c0b1fa7d1d2c78c3b3547f8bfd
sharaths21312 <62636355+sharaths21312@users.noreply.github.com>
2023-06-27 13:45:24 +0530

    Create footer common to all files

---
+++ js/footerinclude.js
@@ -0,0 +1,40 @@
+const footerTemplate = document.createElement('template');
+
+footerTemplate.innerHTML = `
+<div class="footer-main">
+<div class="footer-text" id="info">
+  <h1>Databased</h1>
+  <h4>
+    <a class="footer-link" href="https://csa.iisc.ac.in">
+      The Department of Computer Science and Automation (CSA)
+    </a>
+    <br />
+  </h4>
+  <h5>
+    <a class="footer-link" href="https://iisc.ac.in">
+      Indian Institute of Science, Bengaluru - 560012, INDIA.
+    </a>
+  </h5>
+  <br>
+  <h6>
+    Copyright © <span class="copyright">2023</span> by The Department of Computer Science and Automation, Indian
+    Institute of Science, Bengaluru - 560012, INDIA.
+  </h6>
+</div>
+<div class="footer-logo">
+  <img id="footer-logo" src="/img/Logo_transparent.png" />
+</div>
+</div>
+`;
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = footerTemplate.innerHTML;
+  }
+}
+
+customElements.define('footer-component', Footer);
