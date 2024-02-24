1a2a3cab8318cc8485dc93b65d37cc976d9081ce
sharaths21312 <62636355+sharaths21312@users.noreply.github.com>
2024-02-24 12:24:14 +0530

    Fix realod bug on ttt page

--- pages/open-day/tictactoe/tictactoe.js
+++ pages/open-day/tictactoe/tictactoe.js
@@ -33,13 +33,13 @@ window.onload = function() {
 //Runs on reset button click
 function reset() {
   winner = false;
-  let index;
   document.getElementsByClassName("winScreen")[0].innerHTML = "";
-  for (let i = 0; i < 16; i++) {
-    index = document.getElementById(i);
-    index.innerHTML = "";
-    index.style.backgroundColor = "";
-  }
+  playerTurn = true;
+  Array.from(document.getElementsByClassName("cells"))
+    .forEach(cell => {
+      cell.innerHTML = "";
+      cell.style.backgroundColor = "";
+    });
   board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   winCombos = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11], [12, 13, 14, 15], [0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15], [0, 5, 10, 15], [3, 6, 9, 12]]
 }
